748b06ff3526b2430259549dea739e1cc78b3832
mark.bukovich46@gmail.com <mark.bukovich46@gmail.com>
2022-06-07 13:31:43 -0500

    Edit Email is working now. Removed unnecessary code

--- Startup.cs
+++ Startup.cs
@@ -49,14 +49,6 @@ namespace ExtraB2CPolicyMVC
             services.AddOptions();
             services.Configure<OpenIdConnectOptions>(Configuration.GetSection("AzureAdB2C"));
 
-            /*services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
-            {
-                *//*options.Events ??= new OpenIdConnectEvents();
-                options.Events.OnRedirectToIdentityProvider += OnRedirectToIdentityProviderFunc;
-                options.Events.OnTokenResponseReceived += OnTokenResponseReceivedFunc;
-                options.Events.OnMessageReceived += OnMessageReceivedFunc;*//*
-            });*/
-
             // Create another authentication scheme to handle extra custom policy
             services.AddAuthentication()
                 .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAdB2C2"), "B2C2", "cookiesB2C");
@@ -67,17 +59,6 @@ namespace ExtraB2CPolicyMVC
                     // options.CallbackPath = "/signin-oidc-custom";
                     options.MetadataAddress = "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_DEMO_CHANGESIGNINNAME";
                 });
-
-            /*services.Configure<CookiePolicyOptions>("cookiesB2C", options =>
-                {
-                    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                    options.CheckConsentNeeded = context => true;
-                    options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
-                    // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
-                    options.HandleSameSiteCookieCompatibility();
-
-                    options.Secure = CookieSecurePolicy.Always;
-                });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -112,44 +93,6 @@ namespace ExtraB2CPolicyMVC
                 // Add endpoints for Razor pages
                 endpoints.MapRazorPages();
             });
-        }
-
-        private async Task OnRedirectToIdentityProviderFunc(RedirectContext context)
-        {
-            // var policyID = context.Properties.Items.FirstOrDefault(x => x.Key == "policy").Value;
-            var issuerAddress = context.ProtocolMessage.IssuerAddress;
-
-            if (issuerAddress == "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/b2c_1a_demo_changesigninname/oauth2/v2.0/authorize")
-            {
-                context.Options.MetadataAddress = "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_DEMO_CHANGESIGNINNAME";
-                // context.Options.ConfigurationManager.MetadataAddress = "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_DEMO_CHANGESIGNINNAME";
-                context.Options.Authority = "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/B2C_1A_DEMO_CHANGESIGNINNAME/v2.0";
-                // options.MetadataAddress = "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_DEMO_CHANGESIGNINNAME";
-                var metaDataAddressProperty = context.Options.ConfigurationManager.GetType().GetProperties().FirstOrDefault(x => x.Name.Equals("MetadataAddress"));
-                if (metaDataAddressProperty != null)
-                {
-                    metaDataAddressProperty.SetValue(context.Options.ConfigurationManager, "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_DEMO_CHANGESIGNINNAME");
-                }
-                /*if (context.Options.ConfigurationManager.GetType().GetProperty("MetadataAddress") != null) 
-                {
-                    context.Options.ConfigurationManager.MetadataAddress = "https://markstestorganization1.b2clogin.com/markstestorganization1.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_DEMO_CHANGESIGNINNAME";
-                }*/
-            }
-
-            await Task.CompletedTask.ConfigureAwait(false);
-        }
-
-        private async Task OnTokenResponseReceivedFunc(TokenResponseReceivedContext context)
-        {
-            var c = context;
-
-            await Task.CompletedTask.ConfigureAwait(false);
-        }
-
-        private async Task OnMessageReceivedFunc(MessageReceivedContext context)
-        {
-            var c = context;
-            await Task.CompletedTask.ConfigureAwait(false);
         }
     }
 }
